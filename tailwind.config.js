783bc85937de420303fde840e26c0923b5456142
sakhra <sakhranoor@gmail.com>
2024-09-30 12:46:26 +0500

    FIRST COMMIT

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // Adjust this based on your project structure
+    './**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
